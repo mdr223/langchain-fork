96c9c18b362bd179d6c8c7f6171fad07e4e19a51
Matthew Russo <mdrusso@amazon.com>
2023-07-05 15:33:08 -0400

    trying to get mock args/kwargs

--- redshift_tests/inputs.py
+++ redshift_tests/inputs.py
@@ -1,2 +1,10 @@
+import pytest
+
 # Create S3 bucket input(s)
-create_bucket_input_1 = "Please create an S3 bucket called mrusso-test-bucket"
+@pytest.fixture
+def create_bucket_input_1();
+    return "Please create an S3 bucket called mrusso-test-bucket"
+
+@pytest.fixture
+def create_bucket_input_2();
+    return "Please create an S3 bucket called mrusso-test-bucket in us-west-2"
--- redshift_tests/test_agent.py
+++ redshift_tests/test_agent.py
@@ -1,13 +1,22 @@
 from redshift_tests.fixtures import *
 from redshift_tests.inputs import *
 
+from langchain.tools.aws import *
+
+import json
+import mock
 
 class TestAgent:
 
     def test_basic(self):
         assert True
 
-    def test_create_bucket(self, tools_with_toolsearch, mocker):
-        mocker.patch('langchain.tools.aws.CreateS3Bucket.short_description')
-        _ = tools_with_toolsearch[11].short_description()
-        tools_with_toolsearch[11].short_description.assert_called_once()
+    @mock.patch('langchain.tools.aws.CreateS3Bucket._run')
+    def test_create_bucket(self, agent_chain, mock):
+        # patch tool
+        # mocker.patch('langchain.tools.aws.CreateS3Bucket._run')
+        _ = agent_chain.run(input="Please create a bucket called mrusso-test-bucket")
+        args, kwargs = mock.call_args
+        print(args)
+        print(kwargs)
+        assert 0
