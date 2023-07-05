b229b1165181b0b8281f8e8c6b9f4c157dd86d1c
Matthew Russo <mdrusso@amazon.com>
2023-07-05 15:33:45 -0400

    fixed fixtures

--- redshift_tests/inputs.py
+++ redshift_tests/inputs.py
@@ -2,9 +2,9 @@ import pytest
 
 # Create S3 bucket input(s)
 @pytest.fixture
-def create_bucket_input_1();
+def create_bucket_input_1():
     return "Please create an S3 bucket called mrusso-test-bucket"
 
 @pytest.fixture
-def create_bucket_input_2();
+def create_bucket_input_2():
     return "Please create an S3 bucket called mrusso-test-bucket in us-west-2"
