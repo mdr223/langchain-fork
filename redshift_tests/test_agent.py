65d0c4713b35ef6454803963c4517e4421c30daf
Matthew Russo <mdrusso@amazon.com>
2023-07-05 14:51:12 -0400

    removed unused input; need to convert to fixture later

--- redshift_tests/test_agent.py
+++ redshift_tests/test_agent.py
@@ -7,7 +7,7 @@ class TestAgent:
     def test_basic(self):
         assert True
 
-    def test_create_bucket(self, tools_with_toolsearch, create_bucket_input_1, mocker):
+    def test_create_bucket(self, tools_with_toolsearch, mocker):
         mocker.patch('langchain.tools.aws.CreateS3Bucket.short_description')
         _ = tools_with_toolsearch[11].short_description()
         tools_with_toolsearch[11].assert_called_once()
