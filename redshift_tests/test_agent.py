940ec60e73de6b9459b72e5fb5abd8bb660330be
Matthew Russo <mdrusso@amazon.com>
2023-07-05 14:51:53 -0400

    check mock called on function not class

--- redshift_tests/test_agent.py
+++ redshift_tests/test_agent.py
@@ -10,4 +10,4 @@ class TestAgent:
     def test_create_bucket(self, tools_with_toolsearch, mocker):
         mocker.patch('langchain.tools.aws.CreateS3Bucket.short_description')
         _ = tools_with_toolsearch[11].short_description()
-        tools_with_toolsearch[11].assert_called_once()
+        tools_with_toolsearch[11].short_description.assert_called_once()
