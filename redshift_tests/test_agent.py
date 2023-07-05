69ef8dea883ad57625a09819d125058e42e93212
Matthew Russo <mdrusso@amazon.com>
2023-07-05 15:36:17 -0400

    trying to use pytest mocker

--- redshift_tests/test_agent.py
+++ redshift_tests/test_agent.py
@@ -4,19 +4,18 @@ from redshift_tests.inputs import *
 from langchain.tools.aws import *
 
 import json
-from unittest import mock
 
 class TestAgent:
 
     def test_basic(self):
         assert True
 
-    @mock.patch('langchain.tools.aws.CreateS3Bucket._run')
-    def test_create_bucket(self, agent_chain, mock):
+    # @mock.patch('langchain.tools.aws.CreateS3Bucket._run')
+    def test_create_bucket(self, agent_chain, mocker):
         # patch tool
-        # mocker.patch('langchain.tools.aws.CreateS3Bucket._run')
+        mocker.patch('langchain.tools.aws.CreateS3Bucket._run')
         _ = agent_chain.run(input="Please create a bucket called mrusso-test-bucket")
-        args, kwargs = mock.call_args
+        args, kwargs = mocker.call_args
         print(args)
         print(kwargs)
         assert 0
